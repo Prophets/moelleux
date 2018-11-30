702d5f1d4721f159cfa4d3d5f116b43fcf982a3d
Valentijn Steenhoudt <vsteenhoudt@prophets.be>
2018-11-30 15:35:54 +0100

    removed emailonacid task (not functional yet)

--- gulpfile.js/tasks/acid/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    'mailgun': {
-        'api_key': process.env.MAILGUN_API_KEY,
-        'domain':  process.env.MAILGUN_DOMAIN,
-        'from_address': process.env.MAILGUN_FROM_ADDRESS
-    }
-};
