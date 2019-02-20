46b7db827d562c72937d044c266dc8843bdc7e70
Samuel Joos <sjoos@prophets.be>
2019-02-20 16:00:40 +0100

    refine typo

--- src/core/templates/layouts/default.js
+++ src/core/templates/layouts/default.js
@@ -22,6 +22,7 @@ import MjssStyle from '../components/mjss-style';
 import MailContext from '../context/mail';
 import Grid from '../components/grid';
 import Article from '../partials/article';
+import { H2 } from '../components/typo';
 
 const DefaultHead = ({content, attributes, core, folder}) => (
     <>
@@ -53,6 +54,11 @@ const DefaultLayout = (props) => {
                         return <Block {...data} />
                     })
                 }
+                <MjmlSection>
+                    <MjmlColumn>
+                        <H2>Grid columns 3 justify right</H2>
+                    </MjmlColumn>
+                </MjmlSection>
                 <Grid columns="3" justify="right">
                     {
                         props.content.articles.map(article => (
@@ -62,6 +68,11 @@ const DefaultLayout = (props) => {
                         ))
                     }
                 </Grid>
+                <MjmlSection>
+                    <MjmlColumn>
+                        <H2>Grid columns 3 justify left</H2>
+                    </MjmlColumn>
+                </MjmlSection>
                 <Grid columns="3" justify="left">
                     {
                         props.content.articles.map(article => (
@@ -71,6 +82,11 @@ const DefaultLayout = (props) => {
                         ))
                     }
                 </Grid>
+                <MjmlSection>
+                    <MjmlColumn>
+                        <H2>Grid columns 3 justify stretch</H2>
+                    </MjmlColumn>
+                </MjmlSection>
                 <Grid columns="3" justify="stretch">
                     {
                         props.content.articles.map(article => (
@@ -80,6 +96,11 @@ const DefaultLayout = (props) => {
                         ))
                     }
                 </Grid>
+                <MjmlSection>
+                    <MjmlColumn>
+                        <H2>Grid columns 3 justify center</H2>
+                    </MjmlColumn>
+                </MjmlSection>
                 <Grid columns="3" justify="center">
                     {
                         props.content.articles.map(article => (
