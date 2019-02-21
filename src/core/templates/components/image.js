53c16c61efde2bf9994b535dec8927c1269d9512
Samuel Joos <sjoos@prophets.be>
2019-02-21 14:18:21 +0100

    use useContext hook instead of Consumer tag for images

--- src/core/templates/components/image.js
+++ src/core/templates/components/image.js
@@ -3,20 +3,18 @@ import {
 } from 'mjml-react';
 import MailContext from '../context/mail';
 
-const Image = ({ src, srcset, cssClass, fullWidth, ...otherProps }) => (
-    <MailContext.Consumer>
-        { ({data}) => {
-            return <MjmlImage
-                fluidOnMobile={true}
-                padding="0"
-                src={data.imagesDestination +'/'+ src}
-                srcset={srcset && srcset.trim().split(',').filter(item => item.trim().length !== 0).map((item)=> {
-                    const parts = item.trim().split(' ').filter(item => item !== '');
-                    return `${data.imagesDestination}/${parts[0]} ${parts.slice(1).join(' ')}`;
-                })}
-                {...otherProps} />
-        }}
-    </MailContext.Consumer>
-)
+const Image = ({ src, srcset, cssClass, fullWidth, ...otherProps }) => {
+    const { data } = React.useContext(MailContext);
+    return <MjmlImage
+        fluidOnMobile={true}
+        padding="0"
+        src={data.imagesDestination +'/'+ src}
+        srcset={srcset && srcset.trim().split(',').filter(item => item.trim().length !== 0).map((item)=> {
+            const parts = item.trim().split(' ').filter(item => item !== '');
+            return `${data.imagesDestination}/${parts[0]} ${parts.slice(1).join(' ')}`;
+        })}
+        {...otherProps}
+    />
+}
 
 export default Image;
